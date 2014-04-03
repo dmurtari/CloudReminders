706bc620ad1f26fb42961ec8bdb3a8c3ee9ed015
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2014-04-03 15:42:04 -0600

    Added a simple home page with an email and password field

--- src/com/rabidmongeese/CloudReminders/HomeActivity.java
+++ src/com/rabidmongeese/CloudReminders/HomeActivity.java
@@ -2,14 +2,42 @@ package com.rabidmongeese.CloudReminders;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements View.OnClickListener{
     /**
      * Called when the activity is first created.
      */
+
+    private EditText email, password;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+
+        email = (EditText) findViewById(R.id.etEmail);
+        password = (EditText) findViewById(R.id.etPassword);
+        Button login = (Button) findViewById(R.id.bLogin);
+
+        login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bLogin:
+                //Log the user in
+                break;
+            default:
+                break;
+        }
+
     }
 }
