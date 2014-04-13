4a79a1edf609b9b8da8281d8d83fc6f51626c9ff
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2014-04-13 00:10:14 -0600

    Added all the necessary methods for a functional database, reminders SQLite database now works

--- src/com/rabidmongeese/CloudReminders/ReminderSQLiteHelper.java
+++ src/com/rabidmongeese/CloudReminders/ReminderSQLiteHelper.java
@@ -1,13 +1,21 @@
 package com.rabidmongeese.CloudReminders;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by irakli on 4/12/14.
  */
 public class ReminderSQLiteHelper extends SQLiteOpenHelper {
+
+    private static final String LOG = "DatabaseHelper";
 
     private static final int DATABASE_VERSION = 1;
 
@@ -37,5 +45,93 @@ public class ReminderSQLiteHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_REMINDERS);
 
         onCreate(db);
+    }
+
+    public void addReminder(Reminder reminder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_TEXT, reminder.getText());
+        values.put(KEY_LONGITUDE, reminder.getLongitude());
+        values.put(KEY_LATITUDE, reminder.getLatitude());
+
+        db.insert(TABLE_REMINDERS, null, values);
+        db.close();
+    }
+
+    public Reminder constructReminder(Cursor cursor) {
+        Reminder reminder = new Reminder();
+
+        reminder.setId(cursor.getInt(cursor.getColumnIndex(KEY_ID)));
+        reminder.setText(cursor.getString(cursor.getColumnIndex(KEY_TEXT)));
+        reminder.setLongitude(cursor.getDouble(cursor.getColumnIndex(KEY_LONGITUDE)));
+        reminder.setLatitude(cursor.getDouble(cursor.getColumnIndex(KEY_LATITUDE)));
+
+        return reminder;
+    }
+
+    public Reminder getReminder(long id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT  * FROM " + TABLE_REMINDERS + " WHERE "
+                + KEY_ID + " = " + id;
+
+        Log.e(LOG, query);
+
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        return constructReminder(cursor);
+    }
+
+    public List<Reminder> getAllReminders() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT * FROM " + TABLE_REMINDERS;
+
+        Log.e(LOG, query);
+
+        Cursor cursor = db.rawQuery(query, null);
+        List<Reminder> reminders = new ArrayList<Reminder>();
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            do {
+                Reminder reminder = constructReminder(cursor);
+                reminders.add(reminder);
+            } while (cursor.moveToNext());
+        }
+
+        return reminders;
+    }
+
+    public int getRemindersCount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String countQuery = "SELECT  * FROM " + TABLE_REMINDERS;
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        return cursor.getCount();
+    }
+
+    public int updateReminder(Reminder reminder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_TEXT, reminder.getText());
+        values.put(KEY_LONGITUDE, reminder.getLongitude());
+        values.put(KEY_LATITUDE, reminder.getLatitude());
+
+        return db.update(TABLE_REMINDERS, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(reminder.getId()) });
+    }
+
+    public void removeReminder(long id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_REMINDERS, KEY_ID + " = ?",
+                new String[] { String.valueOf(id) });
     }
 }
