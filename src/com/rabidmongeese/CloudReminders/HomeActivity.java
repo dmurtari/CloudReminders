3b5e8aca47f724bd909d73eb155de212f4b6755a
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2014-04-14 14:51:15 -0600

    Started working on user login, doesn't work properly yet

--- src/com/rabidmongeese/CloudReminders/HomeActivity.java
+++ src/com/rabidmongeese/CloudReminders/HomeActivity.java
@@ -1,70 +1,125 @@
 package com.rabidmongeese.CloudReminders;
 
 import android.app.Activity;
+import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
 
-public class HomeActivity extends Activity implements View.OnClickListener{
-    /**
-     * Called when the activity is first created.
-     */
 
-    private EditText email, password;
+public class HomeActivity extends Activity implements 
+	OnClickListener, ConnectionCallbacks, OnConnectionFailedListener {
+	
+	private static final int RC_SIGN_IN = 0;
+	
+	/* Client to interact with the Google API */
+	private GoogleApiClient mGoogleApiClient;
+	
+	private ConnectionResult mConnectionResult;
+	/* Flag that keeps us from initiating other intents while 
+	 * the PendingIntent is in progress */
+	private boolean mIntentInProgress;
+	private boolean mSignInClicked;
+	
+	private SignInButton bSignIn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-
-        email = (EditText) findViewById(R.id.etEmail);
-        password = (EditText) findViewById(R.id.etPassword);
-        Button login = (Button) findViewById(R.id.bLogin);
-
-        login.setOnClickListener(this);
-
-
-        /*
-        //Test code for database
-        ReminderSQLiteHelper db = new ReminderSQLiteHelper(this);
-        Log.d("Insert", "Inserting...");
-
-        db.addReminder(new Reminder("This is one text", 10.2, 30.4));
-        db.addReminder(new Reminder("This is second text", 14.2, 34.4));
-        db.addReminder(new Reminder("This is third text", 18.2, 38.4));
-        db.addReminder(new Reminder("This is fourth text", 22.2, 42.4));
-        db.addReminder(new Reminder("This is fifth text", 26.2, 46.4));
-
-        // Reading all contacts
-        Log.d("Reading: ", "Reading all contacts..");
-        List<Reminder> reminders = db.getAllReminders();
-
-        for (Reminder rm : reminders) {
-            String log = "Id: "+rm.getId()+" , Text: " + rm.getText() + " , Longitude: "
-                    + rm.getLongitude() + " , Latitude: " + rm.getLatitude();
-            // Writing Contacts to log
-            Log.d("Name: ", log);
-        }
-        */
+        
+        bSignIn = (SignInButton) findViewById(R.id.bSignIn);
+        bSignIn.setOnClickListener(this);
+        
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+		.addConnectionCallbacks(this)
+		.addOnConnectionFailedListener(this).addApi(Plus.API, null)
+		.addScope(Plus.SCOPE_PLUS_LOGIN).build();
+        
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bLogin:
-                //Log the user in
+            case R.id.bSignIn:
+                signIn();
                 break;
             default:
                 break;
         }
 
     }
+    
+    private void signIn() {
+		if (!mGoogleApiClient.isConnected()) {
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+    
+    private void resolveSignInError() {
+    	if (mConnectionResult.hasResolution()) {
+    		try {
+    			mIntentInProgress = true;
+    			mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+    		} catch (SendIntentException e) {
+    			mIntentInProgress = false;
+    			mGoogleApiClient.connect();
+    		}
+    	}
+    }
+
+	protected void onStart() {
+    	super.onStart();
+    	mGoogleApiClient.connect();
+
+    }
+    
+    protected void onStop() {
+    	super.onStop();
+    	if (mGoogleApiClient.isConnected()) {
+    		mGoogleApiClient.disconnect();
+    	}
+    }
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!result.hasResolution()) {
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+					this, 0).show();
+			return;
+		}
+		
+		if (!mIntentInProgress) {
+			mConnectionResult = result;
+			if (mSignInClicked) {
+				resolveSignInError();
+			}
+		}
+		
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mSignInClicked = false;
+		Toast.makeText(this, "User successfuly connected", Toast.LENGTH_LONG).show();
+		//getProfileInformation();
+		
+	}
+
+	@Override
+	public void onConnectionSuspended(int cause) {
+		mGoogleApiClient.connect();
+		
+	}
 }
