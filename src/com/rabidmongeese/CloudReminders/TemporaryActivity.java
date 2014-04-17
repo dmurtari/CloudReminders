b1a1c21aa4cabd1d7d2bc20afb8d005e54c738c9
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2014-04-17 17:16:16 -0600

    Created temporary activities to test on emulator

---
+++ src/com/rabidmongeese/CloudReminders/TemporaryActivity.java
@@ -0,0 +1,14 @@
+package com.rabidmongeese.CloudReminders;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemporaryActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.temporary_activity);
+	}
+
+}
