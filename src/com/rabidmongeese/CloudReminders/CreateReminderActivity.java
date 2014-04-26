9a2d1e40f72214b5e999059f7cbf42ad06c08b99
scriptZilla <david.lowery@colorado.edu>
2014-04-26 15:24:12 -0600

    Form Creation for a New Reminder

---
+++ src/com/rabidmongeese/CloudReminders/CreateReminderActivity.java
@@ -0,0 +1,67 @@
+package com.rabidmongeese.CloudReminders;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Build;
+
+public class CreateReminderActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_reminder);
+
+		EditText reminderName = (EditText) findViewById(R.id.reminderName);
+		EditText reminderAddress = (EditText) findViewById(R.id.reminderAddress);
+		EditText reminderDescrip = (EditText) findViewById(R.id.reminderDescription);
+		
+		Button sumbitButton = (Button) findViewById(R.id.createReminderButton);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.create_reminder, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.create_reminder,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
--- src/com/rabidmongeese/CloudReminders/TemporaryActivity.java
+++ src/com/rabidmongeese/CloudReminders/TemporaryActivity.java
@@ -1,14 +1,37 @@
 package com.rabidmongeese.CloudReminders;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TemporaryActivity extends Activity {
+	
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.temporary_activity);
+        
+        
+        setListeners();
+	}
+	
+	private void setListeners(){
+		
+		Button createReminder = (Button) findViewById(R.id.createReminderButton);
+		
+		createReminder.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(TemporaryActivity.this, CreateReminderActivity.class ));
+			}
+			
+		});
 	}
 
 }
